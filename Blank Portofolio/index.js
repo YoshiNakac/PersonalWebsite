daa8e6ca58763fee137beb6b18ec0d504e7b433b
YoshiNakac <131429818+YoshiNakac@users.noreply.github.com>
2023-08-07 23:54:06 -0700

    Add files via upload

---
+++ Blank Portofolio/index.js
@@ -0,0 +1,32 @@
+// Toggle Icon Navbar
+let menuIcon = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener = () => {
+  menuIcon.classList.toggle("bx-x");
+};
+// Scroll Section
+let sections = document.querySelector("sections");
+let navLinks = document.querySelector("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      // Active navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector(`header nave a ${ id } `).classList.add("active");
+      });
+    }
+  });
+
+  // Sticky Header
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+};
